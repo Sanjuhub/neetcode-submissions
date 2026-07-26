a70186c86b33f30525697be28d2d665aeadb5092
Sanjay <SANJAY14324@GMAIL.COM>
2026-07-26 22:30:04 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        let res = 0;
+
+        for (let i = 0; i <= nums.length; i++) {
+            res ^= i ^ nums[i];
+        }
+        return res;
+    }
+}
