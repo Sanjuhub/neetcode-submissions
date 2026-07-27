6b40cd545c15f975180fd5743cb3fd8310b121e2
Sanjay <SANJAY14324@GMAIL.COM>
2026-07-27 13:03:54 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n) {
+        let ans = 0;
+        for (let i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= n & 1;
+            n >>>= 1;
+        }
+        return ans >>> 0;
+    }
+}
