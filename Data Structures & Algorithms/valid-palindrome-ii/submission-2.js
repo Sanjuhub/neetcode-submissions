69249b6c520d88151fd355d2f20d96821491e659
Sanjay <SANJAY14324@GMAIL.COM>
2026-09-07 16:06:40 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        let l = 0,
+            r = s.length - 1;
+        while (l <= r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+                continue;
+            } else {
+                if (
+                    this.checkPalindrome(s.slice(0, l) + s.slice(l + 1, s.length)) ||
+                    this.checkPalindrome(s.slice(0, r) + s.slice(r + 1, s.length))
+                ) {
+                    l++;
+                    r--;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    checkPalindrome(str) {
+        console.log("-><", str);
+        if (str == [...str].reverse().join("")) {
+            return true;
+        }
+        return false;
+    }
+}
